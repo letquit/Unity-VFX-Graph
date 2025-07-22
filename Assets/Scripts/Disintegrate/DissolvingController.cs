1e9dae95d37b3a0f20697e96ccbdf058cd3003be
letquit <1446444589@qq.com>
2025-07-22 14:59:55 +0800

    Disintegrate
    溶解

---
+++ Assets/Scripts/Disintegrate/DissolvingController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class DissolvingController : MonoBehaviour
+{
+    public SkinnedMeshRenderer skinnedMesh;
+    public float dissolveRate = .0125f;
+    public float refreshRate = .025f;
+
+    private Material[] skinnedMaterials;
+
+    private void Start()
+    {
+        if (skinnedMesh != null)
+            skinnedMaterials = skinnedMesh.materials;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(DissolveCo());
+        }
+    }
+
+    private IEnumerator DissolveCo()
+    {
+        if (skinnedMaterials.Length > 0)
+        {
+            float counter = 0;
+            while (skinnedMaterials[0].GetFloat("_DissolveAmount") < 1)
+            {
+                counter += dissolveRate;
+                for (int i = 0; i < skinnedMaterials.Length; i++)
+                {
+                    skinnedMaterials[i].SetFloat("_DissolveAmount", counter);
+                }
+                yield return new WaitForSeconds(refreshRate);
+            }
+        }
+    }
+}
